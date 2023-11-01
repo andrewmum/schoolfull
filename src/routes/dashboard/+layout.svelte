0ff518f014253456834a0cd132a49508413f039c
Andrewm <36317914+andrewmum@users.noreply.github.com>
2023-10-31 22:20:56 -0400

    add sveltekit nav and update details modal

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+    import { goto } from "$app/navigation";
     import { page } from "$app/stores";
     import AnimatedRoute from "$lib/components/AnimatedRoute.svelte";
     import { db, user, userData } from "$lib/firebase";
     import { auth } from "$lib/firebase";
+    import { redirect } from "@sveltejs/kit";
 
     import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
@@ -10,6 +12,10 @@
         const provider = new GoogleAuthProvider();
         const user = await signInWithPopup(auth, provider);
         console.log(user)
+    }
+    async function signOutAndRedirect(){
+      signOut(auth)
+      goto('/');
     }
   </script>
   
@@ -27,7 +33,7 @@
     </div>
     <div class="navbar-end">
       {#if $user}
-        <button class="btn btn-warning" on:click={() => signOut(auth)}>Sign out</button>
+        <button class="btn btn-warning" on:click={signOutAndRedirect}>Sign out</button>
       {:else}
         <button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
       {/if}
